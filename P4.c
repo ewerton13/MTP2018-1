1ceb69ccfb8b1a8ff931b95f3a5ca5fa42d9b7fd
ewerton13 <37593479+ewerton13@users.noreply.github.com>
2018-06-20 14:40:14 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,26 @@
+//Ewerton Henrique Costa Calmon
+//11711EEL016
+#include <stdio.h>
+
+
+int main()
+{
+	int i=0,j=0;
+	char str1[256],str[256]={48};
+	printf("Digite a frase desejada:       ");
+	scanf("%s",&str1);
+	while (str1[i] != '\0')
+	{
+		if (str1[i] >= '0' )	
+		{
+			if(str1[i] <= '9')
+		{
+			str[j]=str1[i];
+			j++;
+		}
+		i++;
+		}
+	}
+	printf("%s",str);
+	return 0;
+}
